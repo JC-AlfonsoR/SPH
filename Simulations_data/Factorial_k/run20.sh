a98450ab94c8dd0806b4688c6a24de68c1177cac
JC-AlfonsoR <jc.alfonso2004@gmail.com>
2015-04-20 17:47:48 +0200

    Add results from Factorial on k value

--- Simulations_data/Factorial_k/run20.sh
+++
@@ -1,9 +0,0 @@
-echo "Empieza Simulacion" >> r20.sh
-matlab -nodisplay -r "publish('Factorial_k12.m')" &
-echo "k12 --- ok" >> r20.sh
-matlab -nodisplay -r "publish('Factorial_k14.m')" &
-echo "k14 --- ok" >> r20.sh
-matlab -nodisplay -r "publish('Factorial_k16.m')" &
-echo "k16 --- ok" >> r20.sh
-matlab -nodisplay -r "publish('Factorial_k18.m')" &
-echo "k18 --- ok" >> r20.sh
