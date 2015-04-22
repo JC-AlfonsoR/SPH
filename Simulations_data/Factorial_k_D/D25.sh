48104c1aa37348713a744af3b34996616aabd883
JC-AlfonsoR <jc.alfonso2004@gmail.com>
2015-04-21 22:25:46 -0500

    Setup factorial simmulation with B_G variations

--- Simulations_data/Factorial_k_D/D25.sh
+++ Simulations_data/Factorial_k_D/D25.sh
@@ -1,8 +1,14 @@
 echo "Empiezan las simulaciones"
 
-matlab -nodisplay -r "publish('k5_D25.m')" &
-matlab -nodisplay -r "publish('k10_D25.m')" &
-matlab -nodisplay -r "publish('k15_D25.m')" &
-matlab -nodisplay -r "publish('k20_D25.m')" &
-matlab -nodisplay -r "publish('k25_D25.m')" &
-matlab -nodisplay -r "publish('k30_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k5_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k10_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k15_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k20_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k25_D25.m')" &
+matlab -nodisplay -r "publish('G8e8_k30_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k5_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k10_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k15_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k20_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k25_D25.m')" &
+matlab -nodisplay -r "publish('G8e6_k30_D25.m')" &
